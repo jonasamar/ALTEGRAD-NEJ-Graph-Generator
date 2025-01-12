82fb3111fc2b2d8061eff8c0d4e0ae5dedf8f8a6
noam-sc <13904910+noam-sc@users.noreply.github.com>
2025-01-12 16:58:03 +0100

    update: convolutionnal decoder

--- autoencoder.py
+++ autoencoder.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from torch_geometric.nn import GINConv
+from torch_geometric.nn import GATv2Conv
 from torch_geometric.nn import global_add_pool
 
 # Decoder
@@ -46,19 +46,10 @@ class GIN(torch.nn.Module):
         self.dropout = dropout
         
         self.convs = torch.nn.ModuleList()
-        self.convs.append(GINConv(nn.Sequential(nn.Linear(input_dim, hidden_dim),  
-                            nn.LeakyReLU(0.2),
-                            nn.BatchNorm1d(hidden_dim),
-                            nn.Linear(hidden_dim, hidden_dim), 
-                            nn.LeakyReLU(0.2))
-                            ))                        
-        for layer in range(n_layers-1):
-            self.convs.append(GINConv(nn.Sequential(nn.Linear(hidden_dim, hidden_dim),  
-                            nn.LeakyReLU(0.2),
-                            nn.BatchNorm1d(hidden_dim),
-                            nn.Linear(hidden_dim, hidden_dim), 
-                            nn.LeakyReLU(0.2))
-                            )) 
+        self.convs.append(GATv2Conv(input_dim, hidden_dim//2))
+        self.convs.append(GATv2Conv(hidden_dim//2, hidden_dim))
+        for layer in range(n_layers-2):
+            self.convs.append(GATv2Conv(hidden_dim, hidden_dim))
 
         self.bn = nn.BatchNorm1d(hidden_dim)
         self.fc = nn.Linear(hidden_dim, latent_dim)
